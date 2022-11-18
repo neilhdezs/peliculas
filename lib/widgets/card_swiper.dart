74bfb4f6bcbaec366b7ac56b4fefba3ee5d37559
nhsdam <nhersal281@g.educaand.es>
2022-11-18 14:46:06 +0100

    fin

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -3,16 +3,39 @@ import 'package:flutter_card_swipper/flutter_card_swiper.dart';
 
 import '../models/models.dart';
 
-class CardSwiper extends StatelessWidget {
+class CardSwiper extends StatefulWidget {
   List<Movie> movies;
+  final Function onNextPage;
 
-  CardSwiper({super.key, required this.movies});
+  CardSwiper({super.key, required this.movies, required this.onNextPage});
+
+  @override
+  State<CardSwiper> createState() => _CardSwiperState();
+}
+
+class _CardSwiperState extends State<CardSwiper> {
+  final SwiperController swiperController = SwiperController();
+
+  @override
+  void initState() {
+    super.initState();
+    swiperController.addListener(() {
+      print(swiperController.index);
+      print(widget.movies.length);
+
+      if (swiperController.index! >= widget.movies.length - 2) {
+        print("obtener la siguiente pagina");
+
+        widget.onNextPage();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    if (movies.isEmpty) {
+    if (widget.movies.isEmpty) {
       return Container(
         width: double.infinity,
         height: size.height * 0.5,
@@ -24,16 +47,17 @@ class CardSwiper extends StatelessWidget {
       width: double.infinity,
       height: size.height * 0.5,
       child: Swiper(
-        itemCount: movies.length,
+        itemCount: widget.movies.length,
+        controller: swiperController,
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.width * 0.9,
         itemBuilder: (_, int index) {
-          final Movie movie = movies[index];
+          final Movie movie = widget.movies[index];
 
           return GestureDetector(
-            onTap: () => Navigator.pushNamed(context, 'details',
-                arguments: movie),
+            onTap: () =>
+                Navigator.pushNamed(context, 'details', arguments: movie),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: FadeInImage(
